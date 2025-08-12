93d0ef986366464412f3be967ce77a4f6755b282
abeyafi <abeyafim@gmail.com>
2025-08-12 17:00:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'TO YOU'],
+			strings: [ 'HAPPY BIRTHDAY TO ME!'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
@@ -400,3 +400,4 @@ window.addEventListener( 'resize', function(){
 	
 	ctx.font = opts.charSize + 'px Verdana';
 })
+
